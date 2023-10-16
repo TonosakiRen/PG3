e20f889d4c6ddae999a79c1d0865e042b6d9bff6
TonosakiRen <ren221603@gmail.com>
2023-10-16 10:35:24 +0900

    toriaezu

--- main.cpp
+++ main.cpp
@@ -6,46 +6,47 @@
 #include <Windows.h>
 #include <functional>
 
-int Rand(int min, int max) {
-	return min + (int)(rand() * (max - min + 1.0) / (1.0 + RAND_MAX));
+class Enemy {
+public:
+	void Update();
+	void Approach();
+	void Shoot();
+	void Retreat();
+	enum stats {
+		approach,
+		shoot,
+		retreat,
+
+		statsNum
+	};
+
+private:
+	static void (Enemy::*spFuncTable[])();
+};
+
+void (Enemy::* Enemy::spFuncTable[])() = {
+	&Enemy::Approach,
+	&Enemy::Shoot,
+	&Enemy::Retreat
+};
+
+void Enemy::Update() {
+
+	(this->*spFuncTable[static_cast<size_t>(approach)]);
+
 }
+void Enemy::Approach() {
+	std::cout << std::en;
+}
+void Enemy::Shoot() {
 
-typedef void (*Pfunc)(bool,int);
+}
+void Enemy::Retreat() {
 
-
-void Result(bool isEven, int answer) {
-	int answerRemainder = answer % 2;
-	if (isEven == true && answerRemainder == 0) {
-		std::cout << "正解！" << std::endl;
-	}
-	else if (isEven == true && answerRemainder == 1) {
-		std::cout << "不正解！" << std::endl;
-	}
-	else if (isEven == false && answerRemainder == 0) {
-		std::cout << "不正解！" << std::endl;
-	}
-	else if (isEven == true && answerRemainder == 1) {
-		std::cout << "正解！" << std::endl;
-	}
-	std::cout << "答えは" << answer << std::endl;
 }
 
 int main() {
-	srand(static_cast<unsigned int>(time(nullptr)));
-	while (true) {
-		int randNum = Rand(0, 6);
-		int isEven = 0;
-		while (true) {
-			std::cout << "偶数なら１、奇数なら０を入力してください" << std::endl;
-			scanf_s("%d", &isEven);
-			if (isEven == 0 || isEven == 1) {
-				break;
-			}
-		}
-		std::function<void(int)> fx = [](int second) {Sleep(second * 1000); };
-		fx(3);
-		Result(static_cast<bool>(isEven), randNum);
-	}
+	
 	return 0;
 }
 
