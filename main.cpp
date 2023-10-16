50d5b127232eea43e42fbaa785bdfd74b00e9a7b
TonosakiRen <ren221603@gmail.com>
2023-10-16 10:52:33 +0900

    tienndekimasen

--- main.cpp
+++ main.cpp
@@ -12,14 +12,15 @@ public:
 	void Approach();
 	void Shoot();
 	void Retreat();
-	enum stats {
+	enum Stats {
 		approach,
 		shoot,
 		retreat,
 
 		statsNum
 	};
-
+	Stats stats_ = approach;
+	int frame_ = 0;
 private:
 	static void (Enemy::*spFuncTable[])();
 };
@@ -31,22 +32,36 @@ void (Enemy::* Enemy::spFuncTable[])() = {
 };
 
 void Enemy::Update() {
-
-	(this->*spFuncTable[static_cast<size_t>(approach)]);
+	frame_++;
+	if (frame_ % 60 == 0) {
+		if (static_cast<int>(statsNum) == static_cast<Stats>(static_cast<int>(stats_) + 1)) {
+			stats_ = static_cast<Stats>(0);
+		}
+		else {
+			stats_ = static_cast<Stats>(static_cast<int>(stats_) + 1);
+		}
+	}
+	(this->*spFuncTable[static_cast<size_t>(stats_)])();
 
 }
 void Enemy::Approach() {
-	std::cout << std::en;
+	std::cout << "接近中" << std::endl;
 }
 void Enemy::Shoot() {
-
+	std::cout << "射撃中" << std::endl;
 }
 void Enemy::Retreat() {
-
+	std::cout << "離脱中" << std::endl;
 }
 
 int main() {
-	
+	Enemy enemy;
+	while (true) {
+		enemy.Update();
+		if (enemy.frame_ >= 1000) {
+			break;
+		}
+	}
 	return 0;
 }
 
