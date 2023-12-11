2ea98d39b01eb77b2cdcb18c6de1668a718c7ac3
TonosakiRen <ren221603@gmail.com>
2023-12-11 11:47:53 +0900

    fileopne

--- main.cpp
+++ main.cpp
@@ -5,109 +5,36 @@
 #include <cstdlib>
 #include <Windows.h>
 #include <functional>
-#include <list>
+#include <vector>
 #include <string>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 int main() {
 
-    enum stationEra {
-        era1970 = 1970,
-        era2019 = 2019,
-        era2022 = 2022,
-    };
+	std::string filePath = "PG3_05_02.txt";
 
-    stationEra stationEra = era2022;
+    std::ifstream file(filePath);
 
-    std::list <const char*> stationNames = {
-      "Tokyo",
-      "Kanda",
-      "Akihabara",
-      "Okachimachi",
-      "Ueno",
-      "Uguisudani",
-      "Nippori",
-      "Nishinippori",
-      "Tabata",
-      "Komagome",
-      "Sugamo",
-      "Otsuka",
-      "Ikebukuro",
-      "Mejiro",
-      "Takadanobaba",
-      "Shin-Okubo",
-      "Shinjuku",
-      "Yoyogi",
-      "Harajuku",
-      "Shibuya",
-      "Ebisu",
-      "Meguro",
-      "Gotanda",
-      "Osaki",
-      "Shinagawa",
-      "TakanawaGateway",
-      "Tamachi",
-      "Hamamatsucho",
-      "Shimbashi",
-      "Yurakucho"
-    };
-    while (true) {
-        std::cout << "表示する年代を入力してください(1970,2019,2022)" << std::endl;
-        int era = 0;
-        scanf_s("%d", &era);
-        bool correctEra = true;
-        switch (era)
-        {
-        case era1970:
-        {
-            auto it = std::find(stationNames.begin(), stationNames.end(), "Nishinippori");
-            if (it != stationNames.end()) {
-                stationNames.erase(it);
-            }
-            it = std::find(stationNames.begin(), stationNames.end(), "TakanawaGateway");
-            if (it != stationNames.end()) {
-                stationNames.erase(it);
-            }
-        }
-            break;
-        case era2019:
-        {
-            auto it = std::find(stationNames.begin(), stationNames.end(), "Nishinippori");
-            if (it == stationNames.end()) {
-                it = std::find(stationNames.begin(), stationNames.end(), "Nippori");
-                stationNames.insert(++it, "Nishinippori");
-            }
-            it = std::find(stationNames.begin(), stationNames.end(), "TakanawaGateway");
-            if (it != stationNames.end()) {
-                stationNames.erase(it);
-            }
-        }
-            break;
-        case era2022:
-        {
-            auto it = std::find(stationNames.begin(), stationNames.end(), "Nishinippori");
-            if (it == stationNames.end()) {
-                it = std::find(stationNames.begin(), stationNames.end(), "Nippori");
-                stationNames.insert(++it, "Nishinippori");
-            }
-            it = std::find(stationNames.begin(), stationNames.end(), "TakanawaGateway");
-            if (it == stationNames.end()) {
-                it = std::find(stationNames.begin(), stationNames.end(), "Shinagawa");
-                stationNames.insert(++it, "TakanawaGateway");
-            }
-        }
-            break;
-        default:
-            correctEra = false;
-            break;
-        }
+    std::string line;
 
-        if (correctEra) {
-            for (const auto& name : stationNames) {
-                std::wcout << name << std::endl;
-            }
+    std::vector<std::string> ids;
+
+    while (std::getline(file, line)) {
+       
+        std::istringstream iss(line);
+        std::string id;
+
+        while (std::getline(iss, id, ',')) {
+            ids.push_back(id);
         }
 
     }
+
+    // ファイルを閉じる
+    file.close();
 
 	return 0;
 }
