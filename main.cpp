0cce90db755de1b2d4b282a1493d0097e44aa5d9
TonosakiRen <ren221603@gmail.com>
2024-01-22 10:29:08 +0900

    提出

--- main.cpp
+++ main.cpp
@@ -1,56 +1,19 @@
 #include <stdio.h>
+#include <thread>
 #include <iostream>
-#include <assert.h>
-#include <cmath>
-#include <cstdlib>
-#include <Windows.h>
-#include <functional>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <regex>
+
+void PrintThread(int num) {
+	std::cout << "thread " << num << std::endl;
+}
 
 int main() {
 
-    struct account {
-        std::string id;
-        int era_number;
-    };
-
-	std::string filePath = "PG3_05_02.txt";
-
-    std::ifstream file(filePath);
-
-    std::string line;
-
-    std::vector<account> accounts;
-
-    std::regex pattern(R"(k(\d+)g(\d+)@g.neec.ac.jp)");
-
-    std::string id;
-
-    while (std::getline(file, id, ',')) {
-        std::smatch match;
-        int era_number = 0;
-        if (std::regex_search(id, match, pattern)) {
-            era_number = std::stoi(match[1].str() + match[2].str());
-        }
-        accounts.push_back({ id,era_number });
-    }
-
-    file.close();
-
-    std::sort(accounts.begin(), accounts.end(), [](const account& a, const account& b) {
-        return a.era_number < b.era_number;
-    });
-
-    for (auto& account : accounts) {
-        std::cout << account.id << std::endl;
-    }
-
+	std::thread th1(PrintThread, 1);
+	th1.join();
+	std::thread th2(PrintThread, 2);
+	th2.join();
+	std::thread th3(PrintThread, 3);
+	th3.join();
 	return 0;
 }
 
