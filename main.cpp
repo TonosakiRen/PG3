afeb1dbb39a2140a0b36a2de3a772751a965cc92
TonosakiRen <ren221603@gmail.com>
2024-02-05 16:17:59 +0900

    提出

--- main.cpp
+++ main.cpp
@@ -1,84 +1,21 @@
-#include <stdio.h>
-#include <thread>
-#include <queue>
-#include <condition_variable>
-#include <mutex>
 #include <iostream>
+
+#include <chrono>
 #include <string>
-#include <fstream>
-#include <sstream>
 
-std::vector<std::string> split(std::string& input, char delimiter) {
-	std::istringstream stream(input);
-	std::string field;
-	std::vector<std::string> result;
-	while (getline(stream, field, delimiter)) result.push_back(field);
-	return result;
-}
 int main() {
+    std::string str(1000000, 'a');
+    std::chrono::system_clock::time_point time = std::chrono::system_clock::now();
 
-	std::mutex mutex;
-	std::condition_variable condition;
-	std::queue<int> q;
-	bool exit = false;
-	bool isLoaded = false;
+    std::string copy = str;
+    std::chrono::microseconds copyTime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - time);
+    std::cout << copyTime.count() << std::endl;
 
-	std::vector<std::vector<int>> data;
+    time = std::chrono::system_clock::now();
 
+    std::string move = std::move(str);
+    std::chrono::microseconds moveTime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - time);
+    std::cout << moveTime.count() << std::endl;
 
-	std::thread th([&]() {
-		std::unique_lock<std::mutex> uniqueLock(mutex);
-		while (!exit) {
-			if (q.empty()) {
-				condition.wait(uniqueLock);
-			}
-			else {
-				std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-				q.pop();
-				std::ifstream ifs("map.csv");
-				std::string line;
-
-				while (std::getline(ifs, line)) {
-					std::vector<int> dataVec;
-					std::vector<std::string> strVec = split(line, ',');
-					for (auto&& s : strVec) dataVec.push_back(std::stoi(s));
-					data.push_back(dataVec);
-				}
-
-				isLoaded = true;
-			}
-		}
-	});
-
-
-	//gameループ
-	while (true) {
-		//mapChipの読み込み(非同期)
-		
-		if (!isLoaded) {
-			//読み込み処理
-			std::cout << "NowLoading";
-			q.push(2);
-			condition.notify_all();
-			while (!isLoaded) {
-				std::this_thread::sleep_for(std::chrono::milliseconds(1));
-				std::cout << ".";
-			}
-			std::cout << std::endl;
-		}
-
-		//mapChipの処理
-		for (int y = 0; y < data.size();y++) {
-			for (int x = 0; x < data[y].size();x++) {
-				std::cout << data[y][x] << ",";
-			}
-			std::cout << std::endl;
-		}
-
-		break;
-	}
-	th.join();
-	exit = true;
-	return 0;
+    return 0;
 }
-
